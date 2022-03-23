ca0f460548de4f56cd7820f6406137411a8fa5e5
윤지섭 <@DESKTOP-GLDK1IA>
2022-03-23 11:57:12 +0900

    영환님거 풀하기전에 조금해놓은거 푸쉬

--- pro_A/src/main/java/spring/com/pro_A/member/controller/MemberControllerImpl.java
+++ pro_A/src/main/java/spring/com/pro_A/member/controller/MemberControllerImpl.java
@@ -39,4 +39,46 @@ public class MemberControllerImpl implements MemberController{
 		
 		return mav;
 	}
+	
+	private String getViewName(HttpServletRequest request) throws Exception{
+		// /pro03/test/login.do?id=???
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+			
+		if(uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+		
+		System.out.println("uri : " + uri);
+		
+		int begin = uri.lastIndexOf("/");
+		
+		int end;
+		if(uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		} else {
+			end = uri.length();
+		}
+		
+		String fileName = uri.substring(begin, end);
+		System.out.println(fileName);
+		
+		if(fileName.indexOf(".") != -1) {
+			fileName = fileName.substring(0, fileName.lastIndexOf("."));
+		}
+		System.out.println(fileName);
+
+		return fileName;
+	}
+
+	@Override
+	@RequestMapping(value="/test/memberForm.do")
+	public ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName(getViewName(request));
+		return mav;
+	}
+	
+	
+	
+	
 }
---
+++ pro_A/src/main/webapp/WEB-INF/views/test/memberForm.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/pro_A/test/addMember.do" method="post">
+		<h1 style="text-align:center;">회원가입</h1>
+		<table style="margin:auto;">
+			<tr>
+				<td width="200">아이디</td>
+				<td width="400"><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>비밀번호</td>
+				<td><input type="password" name="pwd"></td>
+			</tr>
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>이메일</td>
+				<td><input type="email" name="email"></td>
+			</tr>
+			<tr>
+				<td colspan="2" style="text-align:center;">
+					<input type="submit" value="회원가입">
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
