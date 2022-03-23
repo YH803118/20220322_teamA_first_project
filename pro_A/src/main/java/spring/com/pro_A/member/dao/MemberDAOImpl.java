e22844d8c6653803d6f760c23873f0a0194006f4
dudgh <dudgh@59.17.243.185>
2022-03-23 11:50:03 +0900

    로그인 기능구현완료

--- pro_A/src/main/java/spring/com/pro_A/member/dao/MemberDAOImpl.java
+++ pro_A/src/main/java/spring/com/pro_A/member/dao/MemberDAOImpl.java
@@ -1,19 +1,21 @@
 package spring.com.pro_A.member.dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import spring.com.pro_A.member.dto.MemberDTO;
 
 @Repository
 public class MemberDAOImpl implements MemberDAO{
-
-	SqlSession sqlSession;
+	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@Override
 	public MemberDTO login(MemberDTO dto) {
 		// TODO Auto-generated method stub
-		return sqlSession.selectOne("mapper.member.loginCheck");
+		return sqlSession.selectOne("mapper.member.loginCheck", dto);
 	}
 
 }
