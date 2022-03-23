0f373fbb0da43512bfd3e0b598b4a54f1b240b87
dudgh <dudgh@59.17.243.185>
2022-03-23 09:34:20 +0900

    0323_0934

---
+++ pro_A/src/main/java/spring/com/pro_A/common/interceptor/ViewNameInterceptor.java
@@ -0,0 +1,56 @@
+package spring.com.pro_A.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ViewNameInterceptor extends HandlerInterceptorAdapter{
+	Logger logger = LoggerFactory.getLogger(ViewNameInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		try {
+			String viewName = getViewName(request);
+			request.setAttribute("viewName", viewName);
+			logger.info(viewName);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	private String getViewName(HttpServletRequest request) throws Exception {
+		String contextPath = request.getContextPath();
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+		if (uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+
+		int begin = 0;
+		if (!((contextPath == null) || ("".equals(contextPath)))) {
+			begin = contextPath.length();
+		}
+
+		int end;
+		if (uri.indexOf(";") != -1) {
+			end = uri.indexOf(";");
+		} else if (uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		} else {
+			end = uri.length();
+		}
+
+		String viewName = uri.substring(begin, end);
+		if (viewName.indexOf(".") != -1) {
+			viewName = viewName.substring(0, viewName.lastIndexOf("."));
+		}
+		if (viewName.lastIndexOf("/") != -1) {
+			viewName = viewName.substring(viewName.lastIndexOf("/",1), viewName.length());
+		}
+		return viewName;
+	}
+}
